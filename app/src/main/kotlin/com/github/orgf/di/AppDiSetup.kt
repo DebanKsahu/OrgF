d08ba5c704452d4c9b1877082564d85ebefa2fae
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-04-11 15:39:16 +0530

    feat(di): add prompt screen Koin module to dependency injection setup

--- app/src/main/kotlin/com/github/orgf/di/AppDiSetup.kt
+++ app/src/main/kotlin/com/github/orgf/di/AppDiSetup.kt
@@ -2,6 +2,7 @@ package com.github.orgf.di
 
 import com.github.orgf.core.di.getCoreKoinModule
 import com.github.orgf.folderpickerscreen.di.getFolderPickerKoinModule
+import com.github.orgf.promptscreen.di.getPromptScreenKoinModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
@@ -15,7 +16,10 @@ fun initKoin(
             getCoreKoinModule(),
 
             // Folder Picker
-            getFolderPickerKoinModule()
+            getFolderPickerKoinModule(),
+
+            // Prompt Screen
+            getPromptScreenKoinModule()
         )
 
 
