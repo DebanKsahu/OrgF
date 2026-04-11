0a1aa86be4a1c7ad0539a040c018cbf49b390abb
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-04-11 23:13:41 +0530

    feat(prompt): add icon resource mapping and string conversion for PromptCategory

--- app/src/main/kotlin/com/github/orgf/utils/enums/PromptCategory.kt
+++ app/src/main/kotlin/com/github/orgf/utils/enums/PromptCategory.kt
@@ -1,9 +1,25 @@
 package com.github.orgf.utils.enums
 
+import com.github.orgf.R
+
 enum class PromptCategory {
     ImageType,
     VideoType,
     DocumentType,
     AudioType,
-    UnknownType
+    UnknownType;
+
+    fun toIconRes(): Int {
+        return when (this) {
+            ImageType -> R.drawable.ic_image
+            DocumentType -> R.drawable.ic_document
+            VideoType -> R.drawable.ic_video
+            AudioType -> R.drawable.ic_audio
+            UnknownType -> R.drawable.ic_folder
+        }
+    }
 }
+
+fun String.toPromptCategoryOrUnknown(): PromptCategory =
+    PromptCategory.entries.firstOrNull { category -> category.name.equals(this, ignoreCase = true) }
+        ?: PromptCategory.UnknownType
