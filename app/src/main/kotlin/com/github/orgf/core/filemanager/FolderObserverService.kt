b405e3504cb14bb0fda4387a0af8a77592ec4321
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-05-14 20:23:43 +0530

    feat(folderobserver): enhance file event handling with improved logging and document file support

--- app/src/main/kotlin/com/github/orgf/core/filemanager/FolderObserverService.kt
+++ app/src/main/kotlin/com/github/orgf/core/filemanager/FolderObserverService.kt
@@ -14,16 +14,14 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.net.toUri
+import androidx.documentfile.provider.DocumentFile
 import com.github.orgf.core.ServiceState
-import com.github.orgf.core.filemanager.models.NewFileEvent
-import com.github.orgf.utils.enums.getFileType
+import com.github.orgf.utils.SELECTED_FOLDER_URI_KEY
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import java.io.File
@@ -35,19 +33,28 @@ class FolderObserverService: Service() {
 
     private val serviceState: ServiceState by inject()
 
+    private val processedFiles = mutableSetOf<String>()
+
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        startForegroundNotification()
+        Log.d("FolderObserverService", "Service onCreate: Foreground notification started")
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val uriString = intent?.getStringExtra("URI_KEY")
+        Log.d("FolderObserverService", "onStartCommand triggered!")
+        val uriString = intent?.getStringExtra(SELECTED_FOLDER_URI_KEY)
 
         if (uriString != null) {
             val uri = uriString.toUri()
             startFolderObserverService(uri)
         }
 
-        startForegroundNotification()
+
         return START_STICKY
     }
 
@@ -58,12 +65,20 @@ class FolderObserverService: Service() {
     }
 
     private fun startFolderObserverService(uri: Uri) {
+        Log.d("FolderObserverService", "Starting for URI: $uri")
         val absoluteFolderPath = toAbsolutePathFromUri(treeUri = uri)
+        Log.d("FolderObserverService", "Resolved Path: $absoluteFolderPath")
 
         val folder = File(absoluteFolderPath)
         if (!folder.exists()) {
+            Log.e(
+                "FolderObserverService",
+                "PATH DOES NOT EXIST or is inaccessible: $absoluteFolderPath"
+            )
             return
         }
+
+        Log.d("FolderObserverService", "Folder exists. Initializing FileObserver...")
 
         folderObserver = @RequiresApi(Build.VERSION_CODES.Q)
         object: FileObserver(
@@ -71,18 +86,36 @@ class FolderObserverService: Service() {
             MOVED_TO or CREATE or CLOSE_WRITE
         ) {
             override fun onEvent(event: Int, fileName: String?) {
+                Log.d("FolderObserverService", "Event: $event, FileName: $fileName")
                 if (fileName==null) return
 
                 val isMoved = (event and MOVED_TO)!=0
                 val isClose = (event and CLOSE_WRITE)!=0
                 if (!isMoved and !isClose) return
 
+                synchronized(processedFiles) {
+                    if (processedFiles.contains(fileName)) return
+                    processedFiles.add(fileName)
+                }
+
                 val fullFilePath = "$absoluteFolderPath/$fileName"
+                Log.d("FolderObserverService", "File Event Detected: $fullFilePath")
                 serviceScope.launch {
+                    val rootFolderUri = uri
+                    val rootDoc =
+                        DocumentFile.fromTreeUri(applicationContext, rootFolderUri) ?: return@launch
+
                     serviceState.emitNewFileEvent(
                         fileName = fileName,
-                        fullFilePath = fullFilePath
+                        fullFilePath = fullFilePath,
+                        rootDoc = rootDoc,
+                        rootFolderUri = rootFolderUri
                     )
+
+                    delay(2000)
+                    synchronized(processedFiles) {
+                        processedFiles.remove(fileName)
+                    }
                 }
             }
         }
