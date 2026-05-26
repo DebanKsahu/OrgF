d21d1c54c9cfae26a6030d820df3505c646654b8
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-05-26 14:20:12 +0530

    refactor(PromptDetail & PromptUpdateDetail): after successfully removed the usage links these 2 data classes are safely deleted.

--- app/src/main/kotlin/com/github/orgf/core/agent/models/PromptDetail.kt
+++
@@ -1,9 +0,0 @@
-package com.github.orgf.core.agent.models
-
-import com.github.orgf.utils.enums.PromptCategory
-
-data class PromptDetail(
-	val category: PromptCategory,
-	val prompt: String,
-	val destinationFolder: String
-)
